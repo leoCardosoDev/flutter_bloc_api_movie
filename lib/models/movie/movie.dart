1d49ca0e7220aabe9094cb702d96f7e8af3dc5fb
Leonardo Cardoso <leocardosodev@gmail.com>
2020-10-15 21:13:22 -0300

    feat: add Movie model

---
+++ lib/models/movie/movie.dart
@@ -0,0 +1,25 @@
+class Movie {
+  final int id;
+  final double popularity;
+  final String title;
+  final String backPoster;
+  final String overview;
+  final String rating;
+
+  Movie({
+    this.id,
+    this.popularity,
+    this.title,
+    this.backPoster,
+    this.overview,
+    this.rating,
+  });
+
+  Movie.fromJson(Map<String, dynamic> json)
+      : id = json[''],
+        popularity = json['popularity'],
+        title = json['title'],
+        backPoster = json['backPoster'],
+        overview = json['overview'],
+        rating = json['vote_average'].toDouble();
+}
